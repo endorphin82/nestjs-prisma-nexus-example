4197f98a193393c2c26a4da9ee2b5b78c95b0931
endorphin82 <123123fuck>
2020-09-06 22:50:17 +0300

    add generate:nexus

--- src/graphql/schema-config.service.ts
+++ src/graphql/schema-config.service.ts
@@ -1,54 +1,24 @@
-import * as path from 'path';
-
 import { Injectable } from '@nestjs/common';
 import { GqlOptionsFactory, GqlModuleOptions } from '@nestjs/graphql';
-
-import { nexusPrismaPlugin } from 'nexus-prisma';
-import { makeSchema } from '@nexus/schema';
 
 import { IncomingMessage } from 'http';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserService } from '../users/services/UserService';
 
-import * as usersSchema from '../users/schema';
-
 import { IUserService } from '../users/services/IUserService';
 import { JWT } from '../common/auth/JWT';
 import { User } from '../users/domain/User';
+import { schema } from './schema-config';
 
 @Injectable()
 export class GraphqlConfigService implements GqlOptionsFactory {
   constructor(
     private readonly prisma: PrismaService,
     private readonly userService: UserService,
-  ) {}
+  ) {
+  }
 
   async createGqlOptions(): Promise<GqlModuleOptions> {
-    const schema = makeSchema({
-      types: { ...usersSchema },
-      plugins: [nexusPrismaPlugin({
-        experimentalCRUD: true,
-        outputs: { typegen: path.join(__dirname, '../generated/nexus-prisma-typegen.ts') },
-      })],
-      typegenAutoConfig: {
-        contextType: 'Context.Context',
-        sources: [
-          {
-            source: '@prisma/client',
-            alias: 'prismaClient',
-          },
-          {
-            source: path.join(__dirname, 'schema-config.service.ts'),
-            alias: 'Context',
-          },
-        ],
-      },
-      outputs: {
-        schema: path.join(__dirname, '../generated/schema.graphql'),
-        typegen: path.join(__dirname, '../generated/nexus.ts'),
-      },
-    });
-
     return {
       schema,
       debug: true,
---
+++ src/graphql/schema-config.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+
+import { nexusPrismaPlugin } from 'nexus-prisma';
+import { makeSchema } from '@nexus/schema';
+
+import * as usersSchema from '../users/schema';
+
+export const schema = makeSchema({
+  types: { ...usersSchema },
+  plugins: [nexusPrismaPlugin({
+    experimentalCRUD: true,
+    outputs: { typegen: path.join(__dirname, '../generated/nexus-prisma-typegen.ts') },
+  })],
+  typegenAutoConfig: {
+    contextType: 'Context.Context',
+    sources: [
+      {
+        source: '@prisma/client',
+        alias: 'prismaClient',
+      },
+      {
+        source: path.join(__dirname, 'schema-config.ts'),
+        alias: 'Context',
+      },
+    ],
+  },
+  outputs: {
+    schema: path.join(__dirname, '../generated/schema.graphql'),
+    typegen: path.join(__dirname, '../generated/nexus.ts'),
+  },
+});
